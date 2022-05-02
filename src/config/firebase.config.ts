ef4847f4c21f2999bfb8139893ba90d0985ce3d4
Sina Monajemi <59583555+smonajemi@users.noreply.github.com>
2022-05-02 01:45:53 -0400

    refacftored

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FirebaseApiKey,
-  authDomain: process.env.REACT_APP_FirebaseAuthDomain,
-  projectId: process.env.REACT_APP_FirebaseProjectId,
-  storageBucket: process.env.REACT_APP_FirebaseStorageBucket,
-  messagingSenderId: process.env.REACT_APP_FirebaseMessagingSenderId,
-  appId: process.env.REACT_APP_FirebaseAppId,
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
 };
